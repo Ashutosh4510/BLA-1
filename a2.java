695b08bf39a31baa275bdddbb5b59d136fb09d67
Ashutosh Dwivedi <ttashu12345@gmail.com>
2025-09-01 08:45:44 +0530

    commit these

---
+++ a2.java
@@ -0,0 +1,9 @@
+public class SamAverage {
+    public static void main(String[] args) {
+        int math = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int average = (math + physics + chemistry) / 3;
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
---
+++ a3.java
@@ -0,0 +1,8 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double mileFactor = 1.6;
+        double miles = km * mileFactor;
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
